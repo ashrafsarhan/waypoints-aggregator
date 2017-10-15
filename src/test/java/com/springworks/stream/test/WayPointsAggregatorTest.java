c4434ab45d9bff34ea3c2535ee7449f31e8ce12d
Ashraf Sarhan <ashraf.sar7an@gmail.com>
2017-10-15 21:25:54 +0200

    Assert on the resulted PointToPointMetric objects from the WayPointsAggregator

--- src/test/java/com/springworks/stream/test/WayPointsAggregatorTest.java
+++ src/test/java/com/springworks/stream/test/WayPointsAggregatorTest.java
@@ -6,6 +6,9 @@ package com.springworks.stream.test;
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -22,12 +25,21 @@ import com.springworks.stream.impl.FileDataFeeder;
 public class WayPointsAggregatorTest {
 
     private IDataProcessor wayPointsAggregator;
+    private List<PointToPointMetric> pointToPointMetrics;
+    private AtomicInteger counter;
     
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
+		counter = new AtomicInteger(0);
+		//Set initial expected PointToPointMetrics
+		pointToPointMetrics = new ArrayList<>();
+		pointToPointMetrics.add(new PointToPointMetric(1, 5000, 0.04, 6.3889, 8.33, 5000, 0.04));
+		pointToPointMetrics.add(new PointToPointMetric(2, 5000, 0.07, 9.4, 8.33, 10000, 0.11));
+		pointToPointMetrics.add(new PointToPointMetric(3, 5000, 0.04, 11.1, 8.33, 15000, 0.15));
+		pointToPointMetrics.add(new PointToPointMetric(4, 5000, 0.04, 8.32, 8.33, 20000, 0.19));
 		//Get file from resources folder
 		ClassLoader classLoader = getClass().getClassLoader();
 		File file = new File(classLoader.getResource("waypoints.json").getFile());
@@ -38,15 +50,12 @@ public class WayPointsAggregatorTest {
 	}
 
 	@Test
-	public void testTheFirstDataEvent() {
-		//Assert on the first data event time diff
-		assertEquals("Test Fails (Not the expected first event)", 5000, ((PointToPointMetric)wayPointsAggregator.streamOutgoingDataEvents().findFirst().get()).getDeltaDurationInSec());	
-	}
-	
-	@Test
-	public void testTheLastDataEvent() {
-		//Assert on the last data event (end event)
-		assertEquals("Test Fails (Not the expected end event)", true, wayPointsAggregator.streamOutgoingDataEvents().filter(e -> e.isEnd()).findFirst().isPresent());	
+	public void testTheResultedPointToPointMetrics() {
+		wayPointsAggregator.streamOutgoingDataEvents().filter(e -> !e.isEnd()).forEach(e -> {
+			System.out.println(e);
+			//Assert on the resulted PointToPointMetric from the WayPointsAggregator processor
+			assertEquals("Test Fails (Not the expected pointToPointMetric " + counter.get() + ")", pointToPointMetrics.get(counter.getAndIncrement()), e);	
+		});
 	}
 
 }
