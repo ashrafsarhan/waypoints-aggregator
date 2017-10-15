5864cc4ebc9bc0251211549c77ea18553221c17d
Ashraf Sarhan <ashraf.sar7an@gmail.com>
2017-10-15 23:49:37 +0200

    Create Bootstrap class with app initializer

---
+++ src/main/java/com/springworks/bootstrap/AppInitializer.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.springworks.bootstrap;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.springworks.common.CsvFileWriter;
+import com.springworks.processor.api.IDataProcessor;
+import com.springworks.processor.impl.WayPointsAggregator;
+import com.springworks.stream.impl.FileDataFeeder;
+
+/**
+ * @author assar
+ *
+ */
+public class AppInitializer {
+
+	// CSV file header
+	private static final String CSV_FILE_HEADER = "id,deltaDurationInSec,deltaDistanceInKm,actualSpeed,speedLimit,accumlatedDurationInSec,accumlatedDistanceInKm";
+	private IDataProcessor wayPointsAggregator;
+
+	public AppInitializer(String fileName) {
+		// Get file from resources folder
+		ClassLoader classLoader = getClass().getClassLoader();
+		File file = new File(classLoader.getResource(fileName).getFile());
+		wayPointsAggregator = new WayPointsAggregator(new FileDataFeeder(file));
+		wayPointsAggregator.start();
+		while (!wayPointsAggregator.isAvailableStream()) {
+		}
+		saveWayPointsMetricsDataToCsvFile();
+	}
+
+	private void saveWayPointsMetricsDataToCsvFile() {
+		try {
+			CsvFileWriter.writeCsvFile("waypoints_metrics.csv", CSV_FILE_HEADER,
+					wayPointsAggregator.streamOutgoingDataEvents());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/main/java/com/springworks/bootstrap/WayPointsAggregatorApp.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.springworks.bootstrap;
+
+/**
+ * @author assar
+ *
+ */
+public class WayPointsAggregatorApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new AppInitializer("waypoints.json");
+	}
+
+}
